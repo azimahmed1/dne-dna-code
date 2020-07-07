54c6fb0e598a965466151f6021dbeb9873450554
azimahmed1 <67243396+azimahmed1@users.noreply.github.com>
2020-07-07 11:29:21 +1000

    Update nested_data.py
    #to read all interface and their IP addrees, one more recursive query is required under interface["ietf-ip:ipv4"]["address"], below code works perfectly
    for interface in json_dict["ietf-interfaces:interfaces"]["interface"]:
    	print("Interface Name:{}".format(interface["name"]))
    	for address in interface["ietf-ip:ipv4"]["address"]:
    		print("IP address:{} \nSubnet Mask:{}".format(address["ip"],address["netmask"]))

--- intro-python/parsing-json/nested_data.py
+++ intro-python/parsing-json/nested_data.py
@@ -16,9 +16,15 @@ with open(os.path.join(here, "interfaces.json")) as file:
 
 # TODO: Loop through the interfaces in the JSON data and print out each
 # interface's name, ip, and netmask.
-for interface in json_data["ietf-interfaces:interfaces"]["interface"]:
-    print("{name}: {ip} {netmask}".format(
-        name=interface["name"],
-        ip=interface["ietf-ip:ipv4"]["address"][0]["ip"],
-        netmask=interface["ietf-ip:ipv4"]["address"][0]["netmask"],
-    ))
+#for interface in json_data["ietf-interfaces:interfaces"]["interface"]:
+#    print("{name}: {ip} {netmask}".format(
+#        name=interface["name"],
+#       ip=interface["ietf-ip:ipv4"]["address"][0]["ip"],
+#        netmask=interface["ietf-ip:ipv4"]["address"][0]["netmask"],
+#    ))
+
+#to read all interface and their IP addrees, one more recursive query is required under interface["ietf-ip:ipv4"]["address"], below code works perfectly
+for interface in json_dict["ietf-interfaces:interfaces"]["interface"]:
+	print("Interface Name:{}".format(interface["name"]))
+	for address in interface["ietf-ip:ipv4"]["address"]:
+		print("IP address:{} \nSubnet Mask:{}".format(address["ip"],address["netmask"]))
